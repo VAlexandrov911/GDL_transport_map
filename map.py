4f340b9b2befd6d2217f01f152363ca658432397
Vasilii Aleksandrov <vasilii_aleksandrov@epam.com>
2022-12-02 16:31:24 -0600

    Add 4th line (macrobus); Update map scheme, using token

--- map.py
+++ map.py
@@ -3,17 +3,20 @@ import plotly.graph_objects as go
 
 
 gdl_metro_stations = pd.read_csv("gdl_metro.csv")
+
 fig = go.Figure(go.Scattermapbox(
     lat=gdl_metro_stations["lat"],
     lon=gdl_metro_stations["lon"],
     mode="markers+text+lines",
     marker=go.scattermapbox.Marker(
         size=14,
-        color=gdl_metro_stations["Line"],
+        symbol=gdl_metro_stations["Type"],
+        # color=gdl_metro_stations["Line"],
     ),
-    text=gdl_metro_stations["Station"],
+    text=gdl_metro_stations["Station"], textposition="bottom right",
 ))
-fig.update_layout(mapbox_style="open-street-map")
+# fig.update_layout(mapbox_style="open-street-map")
+# fig.update_layout(mapbox_style="dark-v11")
 fig.update_layout(margin={"r": 0,
                           "t": 0,
                           "l": 0,
@@ -21,6 +24,7 @@ fig.update_layout(margin={"r": 0,
 fig.update_layout(
     hovermode='closest',
     mapbox=dict(
+    accesstoken='pk.eyJ1IjoiZGFuaWNhbiIsImEiOiJjam4xZjBxZm0zd2FyM3ZwbnFqanJnZG9jIn0.AM7q7GapFhEt_b_JAJVqNA',
         bearing=0,
         center=go.layout.mapbox.Center(
             lat=20.677911291920235,
