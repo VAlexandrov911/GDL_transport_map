39712397712e4ee7fd95f56a2934c3ffc8520016
Vasilii Aleksandrov <vasilii_aleksandrov@epam.com>
2022-12-05 22:50:29 -0600

    Fix imports

--- map.py
+++ map.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import dash_bootstrap_components as dbc
 
 gdl_metro_stations = pd.read_csv("gdl_metro.csv")
